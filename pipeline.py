16c34b88f1ea854eed7b9e5eecfdf339f89a4cad
alexkryu4kov <alexkryu4kov@gmail.com>
2020-04-12 19:03:02 +0700

    Разные параметры для смертей и заражений

--- pipeline.py
+++ pipeline.py
@@ -5,12 +5,13 @@ from extractor.load import load_countries_codes, load_countries_time_series
 from extractor.save import save_predicts_to_csv
 import statsmodels.api as sm
 
-
 START_DATE = date(2020, 4, 5)
 END_DATE = date(2020, 12, 31)
 
 model = sm.tsa.statespace.SARIMAX
-order = (1, 2, 1)  # параметры для ARIMA
+order = (1, 2, 1)  # параметры для confirmed
+
+order_death = (3, 2, 2)  # параметры для deaths
 
 cases_predicts = {}
 death_predicts = {}
@@ -18,16 +19,7 @@ death_predicts = {}
 
 def predict(model, order: tuple, time_series: list) -> list:
     start_step = len(time_series)
-    end_step = start_step + 271
-    model = model(time_series, order=order).fit(disp=False)
-    predicted = model.predict(start_step, end_step)[1:]
-
-    return [int(round(elem)) for elem in predicted]
-
-
-def validation(model, order: tuple, time_series: list):
-    start_step = len(time_series)
-    end_step = start_step + 10
+    end_step = start_step + 264  # количество дней которое нужно запредиктить (с 12 апреля)
     model = model(time_series, order=order).fit(disp=False)
     predicted = model.predict(start_step, end_step)[1:]
 
@@ -35,14 +27,16 @@ def validation(model, order: tuple, time_series: list):
 
 
 confirmed_time_series = load_countries_time_series(TIME_SERIES_CONFIRMED_DATA, COUNTRIES_DATA)
+death_time_series = load_countries_time_series(TIME_SERIES_DEATHS_DATA, COUNTRIES_DATA)
 
 for country, time_series in confirmed_time_series.items():
     cases_predicts[country] = predict(model, order, time_series)  # делаем предикт для каждой страны
 
-death_time_series = load_countries_time_series(TIME_SERIES_DEATHS_DATA, COUNTRIES_DATA)
-
 for country, time_series in death_time_series.items():
-    death_predicts[country] = predict(model, order, time_series)  # делаем предикт для каждой страны
+    try:
+        death_predicts[country] = predict(model, order_death, time_series)  # делаем предикт для каждой страны
+    except Exception as exc:
+        death_predicts[country] = predict(model, (1, 1, 1), time_series)
 
 countries_codes = load_countries_codes(COUNTRIES_DATA)
 
