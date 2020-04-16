99c8ba9317735688cd49c15eddd8337a523b9eee
alexkryu4kov <alexkryu4kov@gmail.com>
2020-04-16 21:57:55 +0700

    Добавлены данные про Россию

--- config/path.py
+++ config/path.py
@@ -2,3 +2,6 @@ TIME_SERIES_CONFIRMED_DATA = 'data/time_series_covid19_confirmed_global.csv'
 TIME_SERIES_DEATHS_DATA = 'data/time_series_covid19_deaths_global.csv'
 COUNTRIES_DATA = 'data/countries.csv'
 SUBMISSION_PATH = 'submission.csv'
+
+TIME_SERIES_CONFIRMED_DATA_RUSSIA = 'data/time_series_covid19_confirmed_RU.csv'
+RUSSIAN_REGIONS_DATA = 'data/russia_regions.csv'
