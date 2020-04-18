0c05881f54f4f2e29bee4ac44df81f5d803b240e
alexkryu4kov <alexkryu4kov@gmail.com>
2020-04-18 22:05:21 +0700

    Вернул данные для всего мира

--- config/constants.py
+++ config/constants.py
@@ -1,2 +1,12 @@
 TOTAL_AMOUNT_OF_PREDICTIONS = 271
 ACTUAL_AMOUNT_OF_PREDICTIONS = 259  # первый предикт - 13 апреля
+
+COUNTRIES_WITH_COLONIES = [
+    'Australia',
+    'Canada',
+    'China',
+    'Denmark',
+    'France',
+    'Netherlands',
+    'United Kingdom',
+]
