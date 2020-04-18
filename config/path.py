e8e0eaa11775b3d1a74af5c55b5e370b65c0310a
alexkryu4kov <alexkryu4kov@gmail.com>
2020-04-18 20:29:02 +0700

    Удалено то что не нужно для России

--- config/path.py
+++ config/path.py
@@ -1,6 +1,3 @@
-TIME_SERIES_CONFIRMED_DATA = 'data/time_series_covid19_confirmed_global.csv'
-TIME_SERIES_DEATHS_DATA = 'data/time_series_covid19_deaths_global.csv'
-COUNTRIES_DATA = 'data/countries.csv'
 SUBMISSION_PATH = 'submission_russia.csv'
 
 TIME_SERIES_CONFIRMED_DATA_RUSSIA = 'data/time_series_covid19_confirmed_RU.csv'
