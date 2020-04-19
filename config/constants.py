8f2cc74c14552ff5e9ecc4d2e67f5dc9ba4dfa82
alexkryu4kov <alexkryu4kov@gmail.com>
2020-04-19 21:45:18 +0700

    Новые данные

--- config/constants.py
+++ config/constants.py
@@ -1,5 +1,5 @@
 TOTAL_AMOUNT_OF_PREDICTIONS = 271
-ACTUAL_AMOUNT_OF_PREDICTIONS = 257  # первый предикт - 13 апреля
+ACTUAL_AMOUNT_OF_PREDICTIONS = 255  # первый предикт - 20 апреля
 
 COUNTRIES_WITH_COLONIES = [
     'Australia',
